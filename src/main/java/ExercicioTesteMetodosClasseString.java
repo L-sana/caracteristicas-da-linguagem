85869cfcefb37c7217547f2f7946ac255ffbe356
Lais Sana <lais.sana@outlook.com>
2021-02-07 10:49:48 -0300

    Exercicio testando metodos da classe String

---
+++ src/main/java/ExercicioTesteMetodosClasseString.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+import java.util.*;
+
+public class ExercicioTesteMetodosClasseString {
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+        String string;
+
+        System.out.println("Exercicio Bootcamp Java Developer Inter na DIO\n");
+        System.out.println("Digite uma string para testar alguns metodos da classe String: ");
+        string = ent.nextLine();
+
+        var charArray= string.toCharArray();
+
+        System.out.println(charArray.length);
+
+        System.out.println("Aula de Java".split(" "));
+        System.out.println(string.concat(" Java "));
+        System.out.println(string.replace("a", "4"));
+        System.out.println(string.replaceAll("[0-9]", "#"));
+
+
+    }
+
+}
